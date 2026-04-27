84274ef0d0bd979c6221b097c70ba390cd0ee651
larifernandes512-rgb <larifernandes512@gmail.com>
2026-04-26 22:14:59 -0300

    Create CadastrarTarefaPage,xaml.cs

---
+++ Views/Dashboards/CadastrarTarefaPage,xaml.cs
@@ -0,0 +1,57 @@
+using StudyFlow.Data;
+using StudyFlow.Data.Models;
+
+namespace StudyFlow.Views;
+
+public partial class CadastrarTarefaPage : ContentPage
+{
+    private readonly StudyFlowDatabaseService _db;
+    private readonly Usuario _usuario;
+
+    public CadastrarTarefaPage(Usuario usuario)
+    {
+        InitializeComponent();
+        _db = new StudyFlowDatabaseService();
+        _usuario = usuario;
+    }
+
+    private async void OnSalvarClicked(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(entryTitulo.Text))
+        {
+            await DisplayAlert("Erro", "Digite um título.", "OK");
+            return;
+        }
+
+        await _db.InitAsync();
+        var professores = await _db.ListarProfessoresAsync();
+
+        var professor = professores.FirstOrDefault(p => p.IdUsuario == _usuario.IdUsuario);
+
+        if (professor == null)
+        {
+            await DisplayAlert("Erro", "Professor não encontrado!", "OK");
+            return;
+        }
+
+        var tarefa = new Tarefa
+        {
+            Titulo = entryTitulo.Text,
+            Descricao = entryDescricao.Text,
+            DataEntrega = dateEntrega.Date,
+            Status = "Pendente",
+            IdProfessor = professor.IdProfessor 
+        };
+
+        await _db.InserirTarefaAsync(tarefa);
+
+        await DisplayAlert("Sucesso", "Tarefa cadastrada!", "OK");
+
+        await Navigation.PopAsync();
+    }
+
+    private async void OnVoltarClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+}
